f51f29b67ad835b36c4f85329a158d2e1f29a7b5
Lego <maja.worren@revolve.no>
2016-10-21 14:40:09 +0200

    Added comments

--- send_mail.py
+++ send_mail.py
@@ -12,19 +12,19 @@ from plot import plot_graph
 
 
 def email(host, trafficDataList,interval):
+	#This function generates and sends an email to the given recipient
+	#using the input taken as parameters
 	smtpObj = smtplib.SMTP(host='smtp.office365.com', port=587)
 	smtpObj.starttls()
-	#smtpObj.login('duytt@ntnu.no', password=getpass.getpass(prompt='Enter your password:'))
 	smtpObj.login('duytt@ntnu.no', 'proCrastinate420')
-	#template = env.get_template('email.html')
-	#text = template.render(traffic_data=traffic_data
+
 
 	msg = MIMEMultipart()
-	#msg = MIMEText('allo from the other side',_subtype='html')
 	msg['Subject'] = "tlaffic flow "
 	msg['From'] = 'duytt@stud.ntnu.no'
 	msg['To'] = 'duytt@stud.ntnu.no'
-
+	
+	#This loop generates plots for all trafficData elements in the trafficDataList
 	for i in range(0,len(host)):
 		graph=plot_graph(host[i],time.time(),trafficDataList[i].getTrafficList(),interval)
 		img_data = open(graph, 'rb').read()
@@ -32,12 +32,12 @@ def email(host, trafficDataList,interval):
 		image = MIMEImage(img_data, name=os.path.basename(graph))
 		msg.attach(image)	
 
-
+	#This is the text that will be displayed in the email
 	text = "Host: "+host[0]+"\n\tip received last "+str(interval*len(trafficDataList[0].getTrafficList()))+" seconds : "+str(trafficDataList[0].getSumOfTrafficList())+" packets\n"+"Host: "+host[1]+"\n\tip received last "+str(interval*len(trafficDataList[1].getTrafficList()))+" seconds : "+str(trafficDataList[1].getSumOfTrafficList())+" packets"
-
+	
 	part1 = MIMEText(text, 'plain')
 	msg.attach(part1)
 
-
+	#This is where the email is sent
 	smtpObj.sendmail(msg['From'], msg['To'], msg.as_string())
 	smtpObj.quit()
