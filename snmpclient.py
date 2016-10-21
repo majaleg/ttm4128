ea80547d2d02d0f62a6e103c067df662432cb24b
Lego <maja.worren@revolve.no>
2016-10-21 14:57:06 +0200

    Added (english) comments
    Pls

--- snmpclient.py
+++ snmpclient.py
@@ -1,11 +1,9 @@
 import time
-from pysnmp.entity.rfc3413.oneliner import ntforg, cmdgen #moduler fra pysnmp
+from pysnmp.entity.rfc3413.oneliner import ntforg, cmdgen #modules from pysnmp
 from moving_window_List_14 import *
-from plot import plot_graph1_4, readTrapLog #for å plotte graf
+from plot import plot_graph1_4, readTrapLog #tused to plot the graph 
 
-
-#129.241.209.8
-
+#Variables used in the class are all given here
 ntfOrg = ntforg.NotificationOriginator()
 cmdGen = cmdgen.CommandGenerator()
 HOST = 'localhost'
@@ -14,18 +12,13 @@ INTERVAL = 5
 windowSize = 1000
 
 def get_datagrams(host='localhost', ipv6=False):
-#def get_datagrams(host):
-
-    """
-    SNMP GET-forespørsel, returnerer respons eller feilmelding.
-    Henter her IP-MIB::ipInReceives og IP-MIB::ipInDelivers
-    http://pysnmp.sourceforge.net/docs/current/apps/sync-command-generator.html
-    """
+    #SNMP GET-request, returns response or error.
+    #http://pysnmp.sourceforge.net/docs/current/apps/sync-command-generator.html
     errorIndication, errorStatus, errorIndex, varBinds = cmdGen.getCmd(
         cmdgen.CommunityData('ttm4128'),
         cmdgen.UdpTransportTarget((host, 161)),
         #http://www.net-snmp.org/docs/mibs/IP-MIB.txt
-        cmdgen.MibVariable('IP-MIB', 'ipInReceives', 0),
+        #Gets IP-MIB::ipInReceives and IP-MIB::ipInDeliverscmdgen.MibVariable('IP-MIB', 'ipInReceives', 0),
         cmdgen.MibVariable('IP-MIB', 'ipInDelivers', 0),
         lookupNames=True, lookupValues=True
     )
@@ -34,10 +27,8 @@ def get_datagrams(host='localhost', ipv6=False):
     return ip_rec, ip_del
 
 def send_trap(ip_delivered, ip_received, host='localhost'):
-    """
-    Sender trap til host med siste lesing av verdiene ip_received og ip_delivered, hentet fra get_datagrams
-    http://pysnmp.sourceforge.net/docs/current/apps/sync-notification-originator.html
-    """
+    #Sends a trap to the host with the last reading of the values ip_received and ip_delivered, retrieved from get_datagrams
+    #http://pysnmp.sourceforge.net/docs/current/apps/sync-notification-originator.html
     ntfOrg.sendNotification(
         ntforg.CommunityData('ttm4128'),
         ntforg.UdpTransportTarget((host, 162)),
@@ -49,6 +40,7 @@ def send_trap(ip_delivered, ip_received, host='localhost'):
 
 
 if __name__ == '__main__':
+    #Main function
     initReceived, initDel = get_datagrams(HOST)
     makeTrafficList(windowSize,int(format(initReceived)))
     while True:
@@ -56,10 +48,13 @@ if __name__ == '__main__':
         updateTrafficList(int(format(ip_received)))
         print(getTrafficList())
         print(getSumOfTrafficList())
+        #Gets the gathered sum of the current trafficList window
         sumWindow=getSumOfTrafficList()
         if sumWindow > THRESHOLD:
+            #If the sum has reached the threshold, send a trap message
             send_trap(ip_received, ip_delivered, HOST)
             print('IP RECEIVED: {}'.format(ip_received))
             print('IP DELIVERED: {}'.format(ip_delivered))
             plot_graph1_4(readTrapLog())
+        #Wait for [interval] nuumber of seconds
         time.sleep(INTERVAL)
