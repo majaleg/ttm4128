f1e266cf5ef3ae34aea6ad7b01f9bed12a13455b
tinduy <duytt@stud.ntnu.no>
2016-11-11 10:57:12 +0100

    task 2.3. a get OSinfo and Interface info

--- cim_client.py
+++ cim_client.py
@@ -1,10 +1,33 @@
 import pywbem 
 
-conn = pywbem.WBEMConnection('http://ttm4128.item.ntnu.no:5988')
-print "hello"
-processes = conn.EnumerateInstances('CIM_Process')
-print processes[0].items()
+conn = pywbem.WBEMConnection('http://ttm4128.item.ntnu.no:5988/cimom')
 
+#processes = conn.EnumerateInstances('CIM_Process')
+#print processes[0].items()
+
+#Gets OS Version
+def get_OS_info():
+	os = conn.EnumerateInstances('CIM_OperatingSystem')[0]
+	os_version = os['Version']
+	print os_version
+	return os_version
+
+def get_ip_interfaces():
+	interface_info=[]
+	interfaces = conn.EnumerateInstances('CIM_IPProtocolEndpoint')
+	#print interfaces.items()
+	for interface in interfaces:
+		print interface['Name']+interface['ipv4address']+interface['subnetmask']
+		interface_info.append({
+			'name':		interface['Name'],
+			'ip_address':	interface['ipv4address'],
+			'subnet_mask':	interface['subnetmask']
+		}) 		
+	return interface_info
+
+
+get_ip_interfaces()
+#IP interfaces
 
 
 
